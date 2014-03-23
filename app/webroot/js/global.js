1ccc8d7559a09faf2441b88f61b6f259b3628e6c
Matt Stephens <matthewstephens@digidev.co.uk>
2014-03-23 12:56:07 +0000

    Fix for sections heights

--- app/webroot/js/global.js
+++ app/webroot/js/global.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  var s = skrollr.init();
+  function adjustHeight(){
+    var s = skrollr.init();
+
+    height = $(window).height();
+
+    if (height<=800) {
+      height = 800;
+    }
+
+    $('section').each(function(){
+      $(this).height(height);
+    })
+
+    s.refresh($('#home'));
+  }
 })
